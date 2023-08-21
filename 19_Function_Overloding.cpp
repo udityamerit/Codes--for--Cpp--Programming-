327e175cdec5a130101f735fbf581e122ee8fd43
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-08-21 07:21:32 +0530

    add new code in cpp

--- 19_Function_Overloding.cpp
+++ 19_Function_Overloding.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int sum(int a, int b){
         return a+b;
 }
+int sum(int a, int b, int c){
+        return a+b+c;
+}
 
 int main(){
-        cout<<
+        cout<<"sum of two number is: "<<sum(3,5)<<endl;
+        cout<<"sum of three number is: "<<sum(4,5,3)<<endl;
+        return 0;
 }
